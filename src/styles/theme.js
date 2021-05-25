21b8cbd579fb92e1b6ef642975949d6dff26543c
Juliano Vendramini Codognos <JulianoVendramini@outlook.com>
2021-05-25 11:11:07 -0300

    setting light and dark modes

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,37 @@
-const lighTheme = {
+import {createGlobalStyle} from 'styled-components';
+
+export const lightTheme = {
+    fontColor: '#000',
     body: '#fff',
-    fontColor: '#000'
+    border: '#000 solid 1px',
+    card: '#f1f1f1',
 }
 
-const darkTheme = {
-    body: '#000',
-    fontColor: '#fff'
+export const darkTheme = {
+    fontColor: '#c9d1d9',
+    body: '#0d1117;',
+    border: '#c9d1d9 solid 1px',
+    card: '#161b22',
 }
+
+export const GlobalStyles = createGlobalStyle`
+
+    body{
+        background-color: ${props => props.theme.body};
+        color: ${props => props.theme.fontColor};
+    }
+
+    .container-card{
+        border: ${props => props.theme.border};
+    }
+    
+    .container-card{
+        background-color: ${props => props.theme.card}
+    }
+
+    .container-card-intro{
+        border-bottom: ${props => props.theme.border};
+        border-top: ${props => props.theme.border};
+    }
+
+`
